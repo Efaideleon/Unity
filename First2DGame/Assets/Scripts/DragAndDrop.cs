700e212633f09b3fb51cda047b27092ee8a81841
Efaideleon <eedeleon@uci.edu>
2023-01-09 13:21:17 -0800

    added table and chair and drag and drop

---
+++ First2DGame/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class DragAndDrop : MonoBehaviour
+{
+
+    private RectTransform rectTransform;
+    private Image image;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+}
