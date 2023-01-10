64b995b4df570a62982f6376f35e863a46af9c51
Efaideleon <eedeleon@uci.edu>
2023-01-09 20:47:28 -0800

    drag and drop

--- First2DGame/Assets/Scripts/DragAndDrop.cs
+++ First2DGame/Assets/Scripts/DragAndDrop.cs
@@ -1,17 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 public class DragAndDrop : MonoBehaviour
 {
+    private bool dragging, placed;
+    private Vector2 originalPosition; 
+    private Vector2 offset;
 
-    private RectTransform rectTransform;
-    private Image image;
-    // Start is called before the first frame update
-
-    void Start()
+    [SerializeField] private Table table1;
+    void Awake()
     {
-        
+        originalPosition = transform.position;
     }
 
+    void Update()
+    {
+        if(placed) return;
+        if(!dragging) return;
+        var mousePosition = GetMousePos();
+        transform.position = mousePosition - offset;
+
+    }
+
+    void OnMouseDown()
+    {
+        dragging = true;
+        offset = GetMousePos() - (Vector2)transform.position;
+        print("mouse is down");
+    }
+
+    void OnMouseUp()
+    {
+        if (Vector2.Distance(transform.position, table1.transform.position) < 5) 
+        {
+            print("placed");
+            Vector3 newPos = new Vector3(-5.09f, 3.19f, 0f);
+            transform.position = newPos;
+            placed = true;
+        }
+        else
+        {
+            transform.position = originalPosition;
+            dragging = false;
+        }
+    }
+    
+    Vector2 GetMousePos(){
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
 }
